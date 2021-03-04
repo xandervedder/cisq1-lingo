be4953ef2ab79fa1be7305271df764c794eef123
Xander Vedder <xander.vedd@gmail.com>
2021-03-04 16:10:23 +0100

    Update calculation of score

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Score.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Score.java
@@ -8,6 +8,9 @@ public class Score {
     }
 
     public Integer calculate() {
+        // If the user didn't do any turns yet, then it should be obvious to not give them any points
+        if (this.numTurns == 0) return 0;
+
         return 5 * (5 - this.numTurns) + 5;
     }
 }
